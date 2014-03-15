ead8e55098120e0273f6c72a24946668fdb7b713
Jordan Robinson <hypahnova@gmail.com>
2014-03-15 19:48:08 +0000

    Adds highlight functionality, and early move functionality.

--- raaOSGSimpleDemo/jrOSGHighlightVisitor.cpp
+++ raaOSGSimpleDemo/jrOSGHighlightVisitor.cpp
@@ -1,6 +1,7 @@
 #include "StdAfx.h"
 #include <windows.h>
 #include <iostream>
+#include <osg/MatrixTransform>
 #include <osgFX/Scribe>
 #include "jrOSGHighlightVisitor.h"
 
@@ -13,14 +14,35 @@ jrOSGHighlightVisitor::~jrOSGHighlightVisitor(void) {
 }
 
 void jrOSGHighlightVisitor::apply(osg::Node &node) {
-	if (node.getName().find("Base") != std::string::npos) {
-		osgFX::Scribe* scribe = new osgFX::Scribe();
-		scribe->setWireframeColor(osg::Vec4(0,1,0,0.5));
-		scribe->addChild(&node);
+	if (node.getName().find("ForeArm.Flat") != std::string::npos) {
+		//std::cout << "highlighting: " << node.getName() << std::endl;
+		//osgFX::Scribe* scribe = new osgFX::Scribe();
+		//osg::Group* parent = node.getParent(0);
+		//
+		//scribe->setName("highlighted");
+		//scribe->setWireframeColor(osg::Vec4(0,1,0,0.5));
+		//scribe->addChild(&node);
+		//
+		//parent->replaceChild(&node, scribe);
 
 		osg::Group* parent = node.getParent(0);
-		parent->replaceChild(&node, scribe);
 
+		osg::MatrixTransform* transform = new osg::MatrixTransform;
+
+		const double angle = 0.1;
+		const osg::Vec3d axis(0, 0, 1);
+		transform->setMatrix(osg::Matrix::rotate(angle, axis));
+		transform->setName("trans");
+		transform->addChild(&node);
+		parent->replaceChild(&node, transform);
+
+	} 	
+	else if (node.getName().find("highlighted") != std::string::npos) {
+		std::cout << "removing highlight on: " << node.getName() << std::endl;
+		osg::Group* parent = node.getParent(0);
+		osgFX::Scribe* scribe = dynamic_cast<osgFX::Scribe*>(&node);
+		parent->replaceChild(&node, scribe->getChild(0));
+		return;
 	}
 
 	traverse(node);
